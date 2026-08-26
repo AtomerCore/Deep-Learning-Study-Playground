ad65316d8f7f8866d9f16d32eebe7d6e3810c83a
AtomerCore <AtomerCore@outlook.com>
2026-08-26 18:28:09 +0800

    feat(course): update "GoogLeNet" course

--- GoogLeNet/main.py
+++ GoogLeNet/main.py
@@ -275,23 +275,3 @@ lr, num_epochs, batch_size = 0.1, 10, 128
 train_iter, test_iter = load_data_fashion_mnist(batch_size, resize=96)
 train_ch6(net, train_iter, test_iter, num_epochs, lr, try_gpu())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
